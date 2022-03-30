3c4a6d56d22542932965e4e20e78369b365d9295
Adrian <adrian.talonia11@gmail.com>
2022-03-29 19:21:35 -0600

    add overriding

--- java-intermediate/src/com/intermediate/oop/polymorphism/OverridingMethod.java
+++ java-intermediate/src/com/intermediate/oop/polymorphism/OverridingMethod.java
@@ -1,4 +1,22 @@
 package com.intermediate.oop.polymorphism;
 
+class Vehicle {
+    //defining a method
+    void run() {
+        System.out.println("Vehicle is running");
+    }
+}
+
+class Bike2 extends Vehicle {
+    @Override
+    void run() {
+        System.out.println("Bike is running safely");
+    }
+}
+
 public class OverridingMethod {
+    public static void main(String[] args) {
+        Bike2 obj = new Bike2();//creating object
+        obj.run();//calling method
+    }
 }
